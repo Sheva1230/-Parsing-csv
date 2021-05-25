089f16d16b5a4978a1615e4907f93aa9deae9fb0
Aleksey Shevtsov <shevax3012@gamil.com>
2021-05-25 12:59:35 +0300

    3.0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import collections
+import openpyxl
 
 '''
 PAYERACCOUNTNUMBER - номер счета плательщика
@@ -7,36 +8,68 @@ PAYEEACCOUNTNUMBER - номер счета получателя
 AMOUNT - сумма операции
 DATEIN - дата операции
 '''
-def write_excel(array,name):
-    data = [i for i in range(len(array))]
-    table_taP = pd.DataFrame.from_dict(array, orient='index')
-    writer = pd.ExcelWriter(name+'.xlsx', engine='xlsxwriter')
-    table_taP.to_excel(writer, sheet_name='Factor_Table')
-    writer.save()
-    writer.close()
+
+header_name = ['PAYERACCOUNTNUMBER', 'PAYEEACCOUNTNUMBER', 'AMOUNT', 'DATEIN']
+
+loaded_table = pd.read_csv('Test_data.csv',sep=';') # читаем таблицу, с помощью параметра sep делим таблицу.
+
+loaded_dict_PAYER = loaded_table['PAYERACCOUNTNUMBER'].to_dict()#преобразуем столбец  PAYERACCOUNTNUMBER в словарь
+loaded_dict_PAYEE = loaded_table['PAYEEACCOUNTNUMBER'].to_dict()#преобразуем столбец PAYEEACCOUNTNUMBER в словарь
+loaded_dict_AMOUNT = loaded_table['AMOUNT'].to_dict()#преобразуем столбец AMOUNT в словарь
+loaded_dict_DATEIN = loaded_table['DATEIN'].to_dict()#преобразуем столбец DATEIN в словарь
+
+def write_header_excel(list_header,lenth,name):
+    wb = openpyxl.load_workbook(filename=name+'.xlsx')
+    names = name.split(' ')
+    ws = wb['Factor_Table']
+    if lenth == 1:
+        if 'Amount' in names and 'Payer' in names:
+            for v in range(0, 3, 2):  # запись в первый столбец наименований дальнейших данных
+                if v == 0:
+                    ws.cell(row=1, column=v+1).value = list_header[v]
+                else:
+                    ws.cell(row=1, column=v).value = list_header[v]
+        elif 'Amount' in names and 'Payee' in names:
+            for v in range(1,3):
+                    ws.cell(row=1, column=v).value = list_header[v]
+        elif 'Operation' in names and 'Payer' in names:
+            for v in range(0, 3, 2):  # запись в первый столбец наименований дальнейших данных
+                if v == 0:
+                    ws.cell(row=1, column=v+1).value = list_header[v]
+                else:
+                    ws.cell(row=1, column=v).value = 'Total operation'
+        elif 'Operation' in names and 'Payee' in names:
+            for v in range(1,3):
+                    ws.cell(row=1, column=v).value = list_header[v]
+
+    wb.save(filename=name+'.xlsx')
+    wb.close()
+
 #Функция на подсчёт общий суммы операций
-def total_amount(dicti,array):
+def total_amount(dicti,array,name):
     total_array = collections.defaultdict(dict)
+    total_array.clear()
     for count in range(len(dicti)):
         for number in array:
             if dicti[count] == number:
                 total_array.setdefault(number, []).append(float(loaded_dict_AMOUNT[count].replace(',', '.')))
     for key in total_array:
         total_array[key] = str(sum(total_array[key]))
-    return total_array
 
-def max_amount(dicti,array):
-    total_array = collections.defaultdict(dict)
+    return write_excel(total_array,name)
+
+def max_amount(dicti,array,name):
+    total_array_max = collections.defaultdict(dict)
     for count in range(len(dicti)):
         for number in array:
             if dicti[count] == number:
-                total_array.setdefault(number, []).append(float(loaded_dict_AMOUNT[count].replace(',', '.')))
-    for key in total_array:
-        total_array[key] = str(max(total_array[key]))
-    return total_array
+                total_array_max.setdefault(number, []).append(float(loaded_dict_AMOUNT[count].replace(',', '.')))
+    for key in total_array_max:
+        total_array_max[key] = str(max(total_array_max[key]))
+    return write_excel(total_array_max, name)
 
 #Функиця на подсчёт	общее количество операций
-def total_operati(dicti,array):
+def total_operati(dicti,array,name):
     total_operatin = collections.defaultdict(dict)
     for number in array:
         total_operatin[number] = 0
@@ -45,7 +78,30 @@ def total_operati(dicti,array):
         for number in array:
             if dicti[count] == number:
                 total_operatin[number] = total_operatin[number] + 1
-    return total_operatin
+
+    return write_excel(total_operatin,name)
+
+def write_excel(array,name):
+    table_taP = pd.DataFrame.from_dict(array, orient='index')
+    size_df = table_taP.shape
+    writer = pd.ExcelWriter(name+'.xlsx', engine='xlsxwriter')
+    table_taP.to_excel(writer, sheet_name='Factor_Table')
+    writer.save()
+    # return (write_header_excel(header_name,size_df[1],name))
+
+def wr_excel_multi(df,name):
+    size_df = df.shape
+    writer = pd.ExcelWriter(name+'.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Factor_Table')
+    writer.save()
+    # return (write_header_excel(header_name, size_df[1], name))
+
+def transpon(array,name):  # функция преобразования из словаря в табличку
+    df = pd.DataFrame.from_dict(array,orient='columns')  # делаем из словаря таблицу
+    index = pd.MultiIndex.from_frame(df)  # делаем мультииндекс таблицу
+    new_df = pd.DataFrame(df.unstack())# создаем таблицу с мультииндексами
+    new_df = new_df.dropna()
+    return wr_excel_multi(new_df,name)
 
 PAYER = []#массив счетов плательщика
 PAYEE = []#массив счетов получателя
@@ -57,18 +113,12 @@ tO_PAYER_PAYEE = collections.defaultdict(dict) #Общая количество 
 tA_PAYER_PAYEE_DATEIN = collections.defaultdict(dict)#Общая сумма операций от плательщика x получателю y за дату z (для каждой пары плательщик-получатель и каждой даты)
 tO_PAYER_PAYEE_DATEIN = collections.defaultdict(dict)#Общая количество операций от плательщика x получателю y за дату z (для каждой пары плательщик-получатель и каждой даты)
 
-loaded_table = pd.read_csv('Test_data.csv',sep=';') # читаем таблицу, с помощью параметра sep делим таблицу.
-
-loaded_dict_PAYER = loaded_table['PAYERACCOUNTNUMBER'].to_dict()#преобразуем столбец  PAYERACCOUNTNUMBER в словарь
-loaded_dict_PAYEE = loaded_table['PAYEEACCOUNTNUMBER'].to_dict()#преобразуем столбец PAYEEACCOUNTNUMBER в словарь
-loaded_dict_AMOUNT = loaded_table['AMOUNT'].to_dict()#преобразуем столбец AMOUNT в словарь
-loaded_dict_DATEIN = loaded_table['DATEIN'].to_dict()#преобразуем столбец DATEIN в словарь
-
 #цикл на заполнение массивов
 for count in range(len(loaded_dict_PAYER)):
     PAYER.append(loaded_dict_PAYER[count])
     PAYEE.append(loaded_dict_PAYEE[count])
     DATEIN.append(loaded_dict_DATEIN[count])
+
     tA_PAYER_PAYEE[loaded_dict_PAYER[count]][loaded_dict_PAYEE[count]] = 0.0
 
     tA_PAYER_PAYEE_DATEIN[loaded_dict_PAYER[count]][loaded_dict_PAYEE[count]] = {}
@@ -112,30 +162,18 @@ DATEIN = set(DATEIN)# с помощью функции set убираем дуб
 # print(tA_PAYER_PAYEE_DATEIN)
 # print(tO_PAYER_PAYEE_DATEIN)
 
-total_amount_PAYER = total_amount(loaded_dict_PAYER,PAYER) #общая сумма операций плательщика
-total_operation_PAYER = total_operati(loaded_dict_PAYER,PAYER)# общие количество операций плательщика
-max_amount_operation_PAYER = max_amount(loaded_dict_PAYER,PAYER)
+total_amount_PAYER = total_amount(loaded_dict_PAYER,PAYER,'a)Total Amount Payer') #общая сумма операций плательщика
+total_operation_PAYER = total_operati(loaded_dict_PAYER,PAYER,'c)Total Operation Payer')# общие количество операций плательщика
+max_amount_operation_PAYER = max_amount(loaded_dict_PAYER,PAYER,'k)Max Amount Payer')
 
-total_amount_PAYEE = total_amount(loaded_dict_PAYEE,PAYEE)#общая сумма операций получателя
-total_operation_PAYEE = total_operati(loaded_dict_PAYEE,PAYEE)#общая количество операций получателя
-max_amount_operation_PAYEE = max_amount(loaded_dict_PAYEE,PAYEE)
+total_amount_PAYEE = total_amount(loaded_dict_PAYEE,PAYEE,'b)Total Amount Payee')#общая сумма операций получателя
+total_operation_PAYEE = total_operati(loaded_dict_PAYEE,PAYEE,'d)Total Operation Payee')#общая количество операций получателя
+max_amount_operation_PAYEE = max_amount(loaded_dict_PAYEE,PAYEE,'l)Max Amount Payee')
 
-total_amount_DATEIN = total_amount(loaded_dict_DATEIN,DATEIN)#общая сумма операций за каждую дату
-total_operation_DATEIN = total_operati(loaded_dict_DATEIN,DATEIN)#общая количество операций за дату
+total_amount_DATEIN = total_amount(loaded_dict_DATEIN,DATEIN,'i)Total Amount DateIn')#общая сумма операций за каждую дату
+total_operation_DATEIN = total_operati(loaded_dict_DATEIN,DATEIN,'j)Total Operation DateIn')#общая количество операций за дату
 
-
-# write_excel(tA_PAYER_PAYEE,'Total Amount Payer Payee')
-
-write_excel(total_amount_PAYER,'a)Total Amount Payer')
-write_excel(total_operation_PAYER,'c)Total Operation Payer')
-write_excel(max_amount_operation_PAYER,'k)Max_amount Payer')
-
-write_excel(total_amount_PAYEE,'b)Total Amount Payee')
-write_excel(total_operation_PAYEE,'d)Total Operation Payee')
-write_excel(max_amount_operation_PAYEE,'l)Max_amount Payee')
-
-write_excel(total_amount_DATEIN,'i)Total amount DateIn')
-write_excel(total_operation_DATEIN,'j)Total operation DateIn')
-
+tA_par_pae = transpon(tA_PAYER_PAYEE,'e)Total amount of transactions from Payer to Payee')#Общая сумма операций от плательщика x получателю y (для каждой пары плательщик-получатель)
+tO_par_pae = transpon(tO_PAYER_PAYEE,'f)Total number of transactions from Payer to Payee')#Общee количество операций от плательщика x получателю y (для каждой пары плательщик-получатель)
 
 print('Всего плательщиков: '+str(len(PAYER))+'\n'+'Всего получателей: ' + str(len(PAYEE)))
